d569473ceb85d77c4a7814ba5ba883e5fe8514f9
MartinDuggan-Defra <Martin.Duggan@Defra.gov.uk>
2020-01-21 17:09:04 +0000

    fixed

--- prac_2b.R
+++ prac_2b.R
@@ -51,9 +51,28 @@ plot(X,col=result$cluster)
 # how many clusters do we need for a clean devide... Well this is unsupervised not supervised.
 # But more clusters the more messy it seems. If not 6. if supervised then number of clusters=number of classes unless bimodal?
 # Clean is subjective...
+
+
+# Crash and burned from here
 par(mfrow=c(3,3))
 numClusters=seq(1:9)
 for(i in 1:9){
 result = kmeans(X,numClusters[i])
 plot(X,col=result$cluster)}
-par(mfrow(c(1,1)))
+par(mfrow=c(1,1))
+
+result = kmeans(X,2)
+plot(X,col=result$cluster)
+## Labbleed and supervised. Actual data is messy... 
+# Real
+plot(iris$Sepal.Length,iris$Sepal.Width,col=iris$Species)
+
+library(dplyr)
+set.seed(100)
+data.train=sample_frac(iris,0.5)
+iris_index <- as.numeric(rownames(data.train))
+data.test <- iris[-iris_index,]
+labels3=data.train[,5]
+results3=knn(train = data.train[,1:4],test = data.test[,1:4],cl=labels3,k=3)
+results3==data.test[,5]
+plot(x=data.test$Sepal.Length,y=data.test$Sepal.Width,col=results3)
